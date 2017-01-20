7b70896fb14f82a687d666f4052339da33b374be
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2017-01-20 10:31:16 +0100

    class typo

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -7,7 +7,7 @@ function init_table(options) {
   var csv_options = options.csv_options || {};
   var datatables_options = options.datatables_options || {};
 
-  $("#" + el).html('<table class="compact striped" id="my-table"></table>');
+  $("#" + el).html('<table class="compact stripe" id="my-table"></table>');
 
   $.when($.get(csv_path)).then(
     function(data){
